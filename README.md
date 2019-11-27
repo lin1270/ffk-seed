4d7b805862926f0c47fa96ec6edcf5c25babe364
linqingchang <linqingchang@linqingchang>
2019-11-27 23:26:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# ffk-seed
+
+> ffk.config.json 
+
+> src/data ---> copy folder
+
+> src/pages --> pages
+
+
+
---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+
+/* eslint-disable */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Index from './index.vue'
+
+new Vue({
+  el: '#app',
+  components: { Index },
+  template: '<Index/>'
+})
