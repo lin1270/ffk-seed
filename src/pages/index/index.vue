bdddadffc43622a7f606912bd864875e59b00d0b
linqingchang <linqingchang@linqingchang>
2020-04-01 12:05:25 +0800

    添加index.vue

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="aaa">gg</div>
+    <div class="bbb">gg</div>
+    <div class="testimg"> </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  mounted() {
+  },
+};
+</script>
+
+<style lang="less">
+@import "./index.less";
+
+.testimg {
+  position: absolute;
+  background: url(~@/img/logo.png);
+  width: 100px;
+  height: 100px;
+  background-size: 100% 100%;
+  z-index: 10000;
+}
+</style>
